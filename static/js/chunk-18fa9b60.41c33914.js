1862cb9eaf429215191d73fa7bc0feead026381f
BSCTools <85211177+BSCToolsMax@users.noreply.github.com>
2022-01-15 15:08:56 +0800

    Add files via upload

---
+++ static/js/chunk-18fa9b60.41c33914.js
@@ -0,0 +1,7 @@
+/*!
+ *  build: vue-admin-beautiful 
+ *  vue-admin-beautiful author: chuzhixin 1204505056@qq.com 
+ *  vue-admin-beautiful QQ Group(QQ群): 972435319、1139183756 
+ *  time: 2022-1-15 15:05:24
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-18fa9b60"],{"37f9":function(n,e,t){"use strict";t.r(e);var r=t("f2bf"),c={class:"index-container"};function o(n,e,t,o,a,b){var i=Object(r["P"])("version-information");return Object(r["H"])(),Object(r["l"])("div",c,[Object(r["p"])(i)])}var a=t("a0b1"),b={name:"Index",components:{VersionInformation:a["default"]}},i=t("6b0d"),s=t.n(i);const u=s()(b,[["render",o]]);e["default"]=u},a0b1:function(n,e,t){"use strict";t.r(e);var r=t("f2bf"),c={style:{}},o=Object(r["m"])("p",null,"Automatically validate code and publish to BSC browser",-1),a=Object(r["m"])("br",null,null,-1),b=Object(r["m"])("p",null," After creation, please check your address in BSC browser and check your token(*) ",-1),i=[o,a,b];function s(n,e){return Object(r["H"])(),Object(r["l"])("div",c,i)}var u=t("6b0d"),l=t.n(u);const d={},f=l()(d,[["render",s]]);e["default"]=f}}]);
